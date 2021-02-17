d3912280e5f1839626d074cbadffe1bbc95c9ac1
SudharsanZen <sudharsanzenith@gmail.com>
2021-02-17 18:44:28 +0530

    deleted glfw

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 import os
-os.system("cmake glfw -DCMAKE_TOOLCHAIN_FILE=glfw/CMake/x86_64-w64-mingw32.cmake .")
+import subprocess as sp
+cmake=sp.Popen(["cmake","../glfw","-DCMAKE_TOOLCHAIN_FILE=glfw/CMake/x86_64-w64-mingw32.cmake ."])
 os.system("premake5.exe vs2019")
